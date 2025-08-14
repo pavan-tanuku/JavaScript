96669de94f513298e722d8c55cf9644f2dee88c1
pavan <pavan.tanuku@sasi.ac.in>
2025-08-14 16:09:33 +0530

    added product & isPositive

---
+++ arrow_functions/positive.js
@@ -0,0 +1,6 @@
+let isPositive = (num) => {
+    let res = num > 0 ? "Positive" : "No";
+    return res;
+}
+
+console.log(isPositive(-10));
---
+++ arrow_functions/product.js
@@ -0,0 +1,3 @@
+let pro = (num1, num2) => num1*num2;
+
+console.log(pro(5, 6)); 
