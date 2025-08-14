d27d6d85c43e1d4ae8f1ba4fbfa02c97dd0001fd
pavan <pavan.tanuku@sasi.ac.in>
2025-08-14 15:53:47 +0530

    added 9th & 10th functions

---
+++ functions_JavaScript/exp10.js
@@ -0,0 +1,6 @@
+// Write a function getRandomNumber that returns a random number between 1 and 100.
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+console.log(getRandomNumber());
---
+++ functions_JavaScript/exp9.js
@@ -0,0 +1,8 @@
+// Write a function convertToUpperCase that converts all letters in a string to uppercase.
+function upperCase(str) {
+    let upperStr = str.toUpperCase();
+    return upperStr;
+}
+
+let str = "Sasi Institute of Technology and Engineering";
+console.log(upperCase(str));
